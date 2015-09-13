227230af7072480aa0dfbc3adb506b9232e13618
shuyun123 <1197173684@qq.com>
2015-09-13 18:41:48 +0800

    add

---
+++ src/net/anumbrella/notes/ui/MainActivity.java
@@ -0,0 +1,14 @@
+package net.anumbrella.notes.ui;
+
+import android.app.Activity;
+
+/**
+ * @author anumbrella
+ * 
+ * @date 2015-9-13 上午9:41:55
+ * 
+ *       主引导activity
+ */
+public class MainActivity extends Activity {
+
+}
