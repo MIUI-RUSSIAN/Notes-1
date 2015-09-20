eba7bc3089ee47a12519dc7223db86806d70d5e1
shuyun123 <1197173684@qq.com>
2015-09-20 12:21:23 +0800

    add

--- gen/com/example/miui_notes/R.java
+++ gen/com/example/miui_notes/R.java
@@ -10,30 +10,43 @@ package com.example.miui_notes;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int tv_title_bar=0x7f040000;
+    }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Example customization of dimensions originally defined in res/values/dimens.xml
          (such as screen margins) for screens with more than 820dp of available width. This
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f030000;
-        public static final int activity_vertical_margin=0x7f030001;
+        public static final int activity_horizontal_margin=0x7f050001;
+        public static final int text_font_size_medium=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int list_background=0x7f020001;
+        public static final int new_note=0x7f020002;
+        public static final int new_note_normal=0x7f020003;
+        public static final int new_note_pressed=0x7f020004;
+        public static final int title_bar_bg=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f090003;
+        public static final int btn_new_note=0x7f090002;
+        public static final int notes_list=0x7f090001;
+        public static final int tv_title_bar=0x7f090000;
+    }
+    public static final class layout {
+        public static final int main_activity=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -57,10 +70,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f070001;
     }
 }
